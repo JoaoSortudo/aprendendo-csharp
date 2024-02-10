f3284ea7643545f9baa28749be295f6ac7646774
João Sortudo <luckyjoey13@gmail.com>
2024-02-10 05:33:14 -0300

    Arquivos do curso

---
+++ 2 - Criando variáveis/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Programa { 
+    static void Main(string[] args)
+    {
+        int idade = 23;
+        int novaIdade = idade + 1;
+        int idadeDosAlunos = [1, 3, 4];
+
+        Console.WriteLine("Olá, mundo! Eu tenho " + idade + " anos. Em abril, farei "+ novaIdade +" anos!");
+
+    }
+}
+
---
+++ 6 - Atribuições de variáveis/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Quantos anos você tem?");
+        int idade = int.Parse(Console.ReadLine());
+
+        if (idade >= 18)
+        {
+            Console.WriteLine("Pode entrar!");
+        }
+        else
+        {
+            Console.WriteLine("Não pode entrar.");
+        }
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
